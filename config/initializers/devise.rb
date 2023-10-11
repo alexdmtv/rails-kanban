24076fa4c9d6c5cded8baf2c126c56cc413c2bb3
Alex Dmitriev <alexey.dmitriev6238@gmail.com>
2023-10-11 11:33:19 -0700

    Move github creds to env vars

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -311,6 +311,6 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
 
-  config.omniauth :github, Rails.application.credentials.github[:client_id],
-                  Rails.application.credentials.github[:secret], scope: 'user'
+  config.omniauth :github, ENV.fetch('GITHUB_CLIENT_ID'),
+                  ENV.fetch('GITHUB_SECRET'), scope: 'user'
 end
