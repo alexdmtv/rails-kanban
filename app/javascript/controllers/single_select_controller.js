3721584d416e48fd24eb93f979c6de98482cd491
Alex Dmitriev <alexey.dmitriev6238@gmail.com>
2023-10-27 12:20:12 -0700

    Remove select elements appended to the body after main select is removed

--- app/javascript/controllers/single_select_controller.js
+++ app/javascript/controllers/single_select_controller.js
@@ -1,26 +1,30 @@
-import { Controller } from '@hotwired/stimulus';
+import {Controller} from '@hotwired/stimulus';
 import SlimSelect from 'slim-select';
 
 // Connects to data-controller="single-select"
 export default class extends Controller {
-	connect() {
-		this.select = new SlimSelect({
-			select: this.element,
-			settings: {
-				showSearch: false,
-				openPosition: 'auto',
-			},
-			events: {
-				afterChange: (newVal) => {
-					this.element.value = newVal[0].value;
-					this.element.dispatchEvent(
-						new Event('input', {
-							bubbles: true,
-							cancelable: true,
-						})
-					);
-				},
-			},
-		});
-	}
+    connect() {
+        this.select = new SlimSelect({
+            select: this.element,
+            settings: {
+                showSearch: false,
+                openPosition: 'auto',
+            },
+            events: {
+                afterChange: (newVal) => {
+                    this.element.value = newVal[0].value;
+                    this.element.dispatchEvent(
+                        new Event('input', {
+                            bubbles: true,
+                            cancelable: true,
+                        })
+                    );
+                },
+            },
+        });
+    }
+
+    disconnect() {
+        this.select.destroy()
+    }
 }
