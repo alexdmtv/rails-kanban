0b0fafff78be0e34afec94d4059f70a3d4f321b0
Alex Dmitriev <alexey.dmitriev6238@gmail.com>
2023-10-30 15:03:04 -0700

    Fix flash controller

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -1,36 +1,36 @@
-import { Controller } from '@hotwired/stimulus';
-import { enter, leave } from 'el-transition';
+import {Controller} from '@hotwired/stimulus';
+import {enter, leave} from 'el-transition';
 
 export default class extends Controller {
-	connect() {
-		enter(this.element).then(() => {
-			this.timer = setTimeout(() => {
-				this.dismiss();
-			}, 3000);
-		});
+    connect() {
+        enter(this.element).then(() => {
+            this.timer = setTimeout(() => {
+                this.dismiss();
+            }, 3000);
+        });
 
-		this.element.addEventListener('mouseenter', this.pauseTimer);
-		this.element.addEventListener('mouseleave', this.resumeTimer);
-	}
+        this.element.addEventListener('mouseenter', this.pauseTimer.bind(this));
+        this.element.addEventListener('mouseleave', this.resumeTimer.bind(this));
+    }
 
-	disconnect() {
-		this.element.removeEventListener('mouseenter', this.pauseTimer);
-		this.element.removeEventListener('mouseleave', this.resumeTimer);
-	}
+    disconnect() {
+        this.element.removeEventListener('mouseenter', this.pauseTimer.bind(this));
+        this.element.removeEventListener('mouseleave', this.resumeTimer.bind(this));
+    }
 
-	pauseTimer = () => {
-		clearTimeout(this.timer);
-	};
+    pauseTimer() {
+        clearTimeout(this.timer);
+    };
 
-	resumeTimer = () => {
-		this.timer = setTimeout(() => {
-			this.dismiss();
-		}, 3000);
-	};
+    resumeTimer() {
+        this.timer = setTimeout(() => {
+            this.dismiss();
+        }, 3000);
+    };
 
-	dismiss = () => {
-		leave(this.element).then(() => {
-			this.element.remove();
-		});
-	};
+    dismiss() {
+        leave(this.element).then(() => {
+            this.element.remove();
+        });
+    };
 }
