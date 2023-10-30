8a0cd0ef79d14d1d9cebb37652f2f1450ca53085
Alex Dmitriev <alexey.dmitriev6238@gmail.com>
2023-10-30 15:12:43 -0700

    Fix modal controller

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -6,29 +6,29 @@ export default class extends Controller {
     static targets = ['backdrop', 'dialogPanel', 'closeBtn'];
 
     connect() {
-        document.addEventListener('keydown', this.handleKeydown);
-        document.addEventListener('click', this.handleCloseClick);
+        document.addEventListener('keydown', this.handleKeydown.bind(this));
+        document.addEventListener('click', this.handleCloseClick.bind(this));
         this.open();
     }
 
     disconnect() {
-        document.removeEventListener('keydown', this.handleKeydown);
-        document.removeEventListener('click', this.handleCloseClick);
+        document.removeEventListener('keydown', this.handleKeydown.bind(this));
+        document.removeEventListener('click', this.handleCloseClick.bind(this));
     }
 
-    handleKeydown = (e) => {
+    handleKeydown(e) {
         if (e.key === 'Escape') {
             this.close();
         }
     };
 
-    handleCloseClick = (e) => {
+    handleCloseClick(e) {
         if (!this.dialogPanelTarget.contains(e.target)) {
             this.close();
         }
     };
 
-    close = async (e) => {
+    async close(e) {
         e?.preventDefault();
         // this.element.classList.add('hidden')
         leave(this.backdropTarget);
@@ -42,7 +42,7 @@ export default class extends Controller {
         this.element.remove();
     };
 
-    open = async (e) => {
+    async open(e) {
         e?.preventDefault();
         // this.element.classList.remove('hidden')
         enter(this.backdropTarget);
